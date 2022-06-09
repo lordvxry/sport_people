12c377653d972bc8209d1342cfe338c13565435a
Vladimir Smolin <lordvxry@gmail.com>
2022-06-10 01:43:02 +0700

    sport_people

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,15 +6,24 @@ import { NavLink } from "react-router-dom";
 import Post from "./Post";
 
 const MainPage = (props) => {
-  const { onConnectToPostById, onDisconnectToPostById } = props;
+  const {
+    onConnectToPostById,
+    onDisconnectToPostById,
+    peopleCountHandbook,
+    priceHandbook,
+    levelHandbook,
+  } = props;
   const [sport, setSport] = useState("");
+  const [count, setCount] = useState("");
+  const [price, setPrice] = useState("");
+  const [level, setLevel] = useState("");
 
   function addFilter() {
-    props.filter(sport)
+    props.filter(sport, count, price, level);
     // console.log(sport);
   }
-
-  const postsElement = props.postData.posts.map((post) => {
+  console.log("cccc", sport);
+  const postsElement = props.posts.map((post) => {
     return (
       <Post
         post={post}
@@ -29,39 +38,62 @@ const MainPage = (props) => {
     <div className={s.main}>
       <div className={s.dropDowns}>
         <div>
-          <select value={sport}  onChange={(event) => setSport(event.target.value)}>
-            <option value="">Вид спорта</option>
+          <select
+            value={sport}
+            onChange={(event) => setSport(event.target.value)}
+          >
+            <option value="all">Вид спорта</option>
             {props.postData.sports.map((sport) => {
+              return <option key={sport.id}>{sport.title}</option>;
+            })}
+          </select>
+        </div>
+        <div>
+          <select
+            name="category-list"
+            id="category-list"
+            value={level}
+            onChange={(event) => setLevel(event.target.value)}
+          >
+            {levelHandbook.map((levelObj, index) => {
               return (
-                <option
-                  key={sport.id}
-                >
-                  {sport.title}
+                <option key={index} value={JSON.stringify(levelObj)}>
+                  {levelObj.title}
                 </option>
               );
             })}
           </select>
         </div>
         <div>
-          <select name="category-list" id="category-list">
-            <option value="">Уровень</option>
-            <option value="Outdoor">Уровень: 1/5</option>
-            <option value="Indoor">Уровень: 2/5</option>
+          <select
+            name="category-list"
+            id="category-list"
+            value={count}
+            onChange={(event) => setCount(event.target.value)}
+          >
+            {peopleCountHandbook.map((countObj, index) => {
+              return (
+                <option key={index} value={JSON.stringify(countObj)}>
+                  {countObj.title}
+                </option>
+              );
+            })}
           </select>
         </div>
         <div>
-          <select name="category-list" id="category-list">
-            <option value="">Количество игроков</option>
-            <option value="Outdoor">до 6 чел.</option>
-            <option value="Indoor">до 10 чел.</option>
-            <option value="Indoor">до 14 чел.</option>
-          </select>
-        </div>
-        <div>
-          <select name="category-list" id="category-list">
-            <option value="">Стоимость</option>
-            <option value="Outdoor">до 500 руб.</option>
-            <option value="Indoor">от 500 до 1000 руб.</option>
+          <select
+            name="category-list"
+            id="category-list"
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+          >
+            {priceHandbook.map((priceObj, index) => {
+              return (
+                <option key={index} value={JSON.stringify(priceObj)}>
+                  {priceObj.title}
+                </option>
+              );
+            })}
           </select>
         </div>
         <div className={s.search}>
