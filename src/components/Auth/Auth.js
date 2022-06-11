edc80e40d644dfc0687c0b5cc1264c820ed5ccca
Vladimir Smolin <lordvxry@gmail.com>
2022-06-11 18:17:50 +0700

    sport_people

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./Auth.module.css";
 import { NavLink } from "react-router-dom";
+import { useHttp } from "../../hooks/http.hook";
 
 const Auth = () => {
+  const { loading, error, request, clearError } = useHttp();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    error;
+    setTimeout(() => {
+      clearError();
+    }, 5000);
+  }, [error, clearError]);
+
+  console.log(error);
+
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const loginHandler = async () => {
+    try {
+      const data = await request("api/auth/login", "POST", { ...form });
+      console.log("Data", data);
+    } catch (err) {}
+  };
+
   return (
     <div className={s.content}>
       <div className={s.auth}>
+        <div className={s.errorMessage}>
+          {error === null ? <div></div> : <div>{error}</div>}
+        </div>
         <div>
           <div className={s.authText}>Логин:</div>{" "}
           <input
+            name="email"
             className={s.authInput}
             placeholder="Введите email"
-            name="email"
+            onChange={changeHandler}
           ></input>
         </div>
         <div>
@@ -21,12 +52,15 @@ const Auth = () => {
             type="password"
             placeholder="Введите пароль"
             className={s.authInput}
+            onChange={changeHandler}
           ></input>
         </div>
         <div className={s.authBtns}>
-          <NavLink to={"/main"}>
-            <button>Войти</button>
-          </NavLink>
+          {/* <NavLink to={"/main"}> */}
+          <button disabled={loading} onClick={loginHandler}>
+            Войти
+          </button>
+          {/* </NavLink> */}
           <NavLink to={"/reg"}>
             <button>Регистрация</button>
           </NavLink>
